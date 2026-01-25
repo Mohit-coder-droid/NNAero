4685957bc05b3d8fc9ee15bf6ff5a9163f41e57b
Mohit-coder-droid <mohitsahuandmohitsahu@gmail.com>
2026-01-25 17:05:45 +0530

    Added some utils for neuralFoil modifications

--- nnaero/utils/__init__.py
+++ nnaero/utils/__init__.py
@@ -2,3 +2,4 @@ from nnaero.utils.trig import *
 from nnaero.utils.array import *
 from nnaero.utils.spacing import *
 from nnaero.utils.rotations import *
+from nnaero.utils.surrogate_model_tools import *
